9cb7801c10ecafd8dc2b06cc19eeaef31ff3a4c6
ChrisColeTech <moneymagnet216@gmail.com>
2024-08-14 01:00:38 -0400

    initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from line_counter import TextFileLineCounter
+
+
+NODE_CLASS_MAPPINGS = {
+    "TextFileLineCounter": TextFileLineCounter,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "TextFileLineCounter": "🤑 Text File Line Counter",
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
