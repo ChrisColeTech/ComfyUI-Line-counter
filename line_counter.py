9e196c28b38cd79d822abd4081d53798c21647c5
ChrisColeTech <moneymagnet216@gmail.com>
2024-08-14 03:27:30 -0400

    bug fixes

--- line_counter.py
+++ line_counter.py
@@ -2,44 +2,29 @@ import os
 class TextFileLineCounter():
     def __init__(self):
         pass
-
-    RETURN_TYPES = ("INT","NUMBER","STRING")  # Ensure this matches ComfyUI's expectations
-    RETURN_NAMES = ()
-    FUNCTION = "count_lines"
-
-    CATEGORY = "line_counter"
+    
     @classmethod
-    def INPUT_TYPES(cls):
+    def INPUT_TYPES(s):
         return {
             "required": {
-                "file_path": ("STRING", {"default": "", "multiline": False}),
+                "file_path": ("STRING", {"default":""}),
             },
         }
 
-    @classmethod
-    def OUTPUT_TYPES(cls):
-        return {
-            "outputs": {
-                "line_count_int": ("INT",),
-                "line_count_number": ("NUMBER",),
-                "line_count_string": ("STRING",),
-            },
-        }
+    RETURN_TYPES = ("INT","FLOAT","STRING",)
 
-    def count_lines(self, file_path: str) -> dict:
+    FUNCTION = "execute_line_counter"
+
+    CATEGORY = "CCTech/Files"
+    
+    def execute_line_counter(self, file_path: str) -> dict:
         if not os.path.isfile(file_path):
             raise FileNotFoundError(f"The file at path '{file_path}' does not exist.")
         with open(file_path, 'r', encoding='utf-8') as file:
             lines = file.readlines()
         line_count = len([line for line in lines if line.strip() != ""]) - 1
-        return {
-            "line_count_int": line_count,           # Integer
-            "line_count_number": float(line_count), # Number (float)
-            "line_count_string": str(line_count)    # String
-        }
-    
-    def test(self):
-        return ()
+        return (line_count, float(line_count),  str(line_count))
+
 
 NODE_CLASS_MAPPINGS = {
 "TextFileLineCounter": TextFileLineCounter,
